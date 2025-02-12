72a1df6ccb261771818b768a84a22d7e043c19a6
Roman Zubakho <rzubakho@gmail.com>
2025-02-12 23:56:18 +0300

    added list

--- resources/js/modules/PropertyData/components/PropertyDataList.vue
+++ resources/js/modules/PropertyData/components/PropertyDataList.vue
@@ -1,6 +1,28 @@
 <template>
-    <div >
-        {{items}}
+    <div class="property-data-list" style="height: 400px; overflow-y: auto;">
+        <el-table
+            :data="items"
+            v-loading="loading"
+            style="width: 100%"
+            height="100%"
+        >
+            <el-table-column prop="name" label="Name" width="200"></el-table-column>
+            <el-table-column prop="bedrooms" label="Bedrooms" width="100"></el-table-column>
+            <el-table-column prop="bathrooms" label="Bathrooms" width="100"></el-table-column>
+            <el-table-column prop="storeys" label="Storeys" width="100"></el-table-column>
+            <el-table-column prop="garages" label="Garages" width="100"></el-table-column>
+            <el-table-column prop="price" label="Price" width="150"></el-table-column>
+
+            <template #empty>
+                <div class="no-data-message">
+                    No data available
+                </div>
+            </template>
+
+            <template #append>
+                <div ref="loadTrigger" class="load-trigger"></div>
+            </template>
+        </el-table>
     </div>
 </template>
 <script>
@@ -15,15 +37,54 @@ export default {
             type: Object,
             required: true,
             default: () => ({})
+        },
+        loading: {
+            type: Boolean,
+            required: true
         }
     },
     data() {
         return {
-
+            observer: null
         };
     },
     methods: {
+        loadMore() {
+            if (this.meta.current_page < this.meta.last_page) {
+                this.$emit("load-more", this.meta.current_page + 1);
+            }
+        },
+        initObserver() {
+            this.observer = new IntersectionObserver(
+                (entries) => {
+                    entries.forEach((entry) => {
+                        if (entry.isIntersecting) {
+                            this.loadMore();
+                        }
+                    });
+                },
+                {threshold: 0.1}
+            );
 
+            const loadTrigger = this.$refs.loadTrigger;
+            if (loadTrigger) {
+                this.observer.observe(loadTrigger);
+            }
+        }
+    },
+    mounted() {
+        this.initObserver();
+    },
+    beforeDestroy() {
+        if (this.observer) {
+            this.observer.disconnect();
+        }
     }
 };
 </script>
+<style scoped>
+.load-trigger {
+    height: 1px;
+    visibility: hidden;
+}
+</style>
--- resources/js/modules/PropertyData/pages/PropertyDataPage.vue
+++ resources/js/modules/PropertyData/pages/PropertyDataPage.vue
@@ -4,6 +4,8 @@
         <Property-data-list
             :items="items"
             :meta="meta"
+            :loading="loading"
+            @load-more="fetchNextPageDebounce"
         />
     </div>
 </template>
@@ -12,6 +14,7 @@
 import PropertyDataList from "@modules/PropertyData/components/PropertyDataList.vue";
 import PropertyDataFilters from "@modules/PropertyData/components/PropertyDataFilters.vue";
 import PropertyDataService from "@/services/property-data.service";
+import {debounce} from "lodash";
 
 export default {
     name: "PropertyDataPage",
@@ -19,24 +22,49 @@ export default {
     data() {
         return {
             items: [],
-            meta: {},
+            errors: [],
+            meta: {
+                page: 1,
+                per_page: 10
+            },
+            loading: false,
             filters: {}
         };
     },
 
     computed: {
-
+        params() {
+            return {...this.filters, page: this.meta.page, per_page: this.meta.per_page};
+        }
     },
 
     methods: {
+        fetchData(filters = {}) {
+            this.items = [];
+            this.filters = { ...filters };
+            this.meta.current_page = 1;
+            this.fetchNextPageDebounce();
+        },
+        fetchNextPageDebounce: debounce(function fetchNextPage(page = this.meta.current_page || 1) {
+            this.loading = true;
+            this.meta.page = page;
 
+            PropertyDataService.getList(this.params)
+                .then(({ data, ...meta }) => {
+                    this.items = [...this.items, ...data];
+                    this.meta = meta;
+                })
+                .catch(error => {
+                    console.log(error);
+                })
+                .finally(() => {
+                    this.loading = false;
+                });
+        }, 300),
     },
+
     created() {
-        PropertyDataService.getList(this.params)
-            .then((data) => {
-                this.items = data.data;
-                this.meta = data;
-            });
+        this.fetchData();
     }
 };
 </script>
